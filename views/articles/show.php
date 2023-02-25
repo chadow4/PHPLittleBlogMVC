4b754fa47c4f97a6ba4786a94fca9d29c04e5ab3
Julien SANCHEZ <chadow.video@gmail.com>
2023-02-25 02:36:55 +0100

    fix responsive

--- views/articles/show.php
+++ views/articles/show.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="card my-5 p-5 col-12 col-md-8 m-auto">
+    <div class="card my-0 my-md-5 p-3 p-md-5 col-12 col-md-8 m-auto">
         <h1 class="fw-lighter text-decoration-underline"><?= $article['title'] ?></h1>
         <p class="mt-3"><?= $article['content'] ?></p>
         <hr>
